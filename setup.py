30b01adae09a9706f505dd1ee12992e79d8143e7
Francesco Di Natale <frank.dinatale1988@gmail.com>
2017-07-18 16:02:13 -0700

    Add ci supportfix (#24)
    * Added tox + travis config files; #22
    
    Signed-off-by: Ian Lee <lee1001@llnl.gov>
    
    * Removal of unsupported Python versions.
    
    * Removal of unsupported Python versions for tox.
    
    * Removal of OS Independent support.
    
    Changed the OS support to Unix because the maestro binary uses the
    Unix 'nohup' command. This command does not have any support on
    Windows platforms.
    
    * Removed unsupported Python versions from travis configuration.
    
    * Removed unsupported Python versions from travis configuration.
    
    Signed-off-by: Francesco Di Natale <dinatale3@llnl.gov>

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(name='maestrowf',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
+        'Operating System :: Unix',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         ],
       )
