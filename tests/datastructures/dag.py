43b676b0e7d7fb68a644ae86aa1a0feddc402789
Francesco Di Natale <frank.dinatale1988@gmail.com>
2018-05-19 20:45:50 -0700

    Shell for DAG tests.

---
+++ tests/datastructures/dag.py
@@ -0,0 +1,20 @@
+"""Unit tests for the maestrowf.datastructures.dag module."""
+import unittest
+
+
+class TestDAG(unittest.TestCase):
+    """Test cases for the DAG datastructure."""
+    def test_add_edge(self):
+        pass
+
+    def test_add_node(self):
+        pass
+
+    def test_dfs(self):
+        pass
+
+    def test_bsf(self):
+        pass
+
+    def test_cycle_detection(self):
+        pass
