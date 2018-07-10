a2a34f853ee93387b0793c12fea0ce2216482cfd
Francesco Di Natale <frank.dinatale1988@gmail.com>
2018-07-09 19:29:45 -0700

    Moved ParallelizeFactory to mpi module.

--- maestrowf/interfaces/__init__.py
+++ maestrowf/interfaces/__init__.py
@@ -29,10 +29,7 @@
 
 """Collection of custom adapters for interfacing with various systems."""
 import logging
-from os.path import abspath, dirname, join
-import yaml
 
-from maestrowf.interfaces.mpi import GeneralParallelizer
 from maestrowf.interfaces.script import \
     LocalScriptAdapter, \
     SlurmScriptAdapter, \
@@ -66,68 +63,3 @@ class ScriptAdapterFactory(object):
     @classmethod
     def get_valid_adapters(cls):
         return cls.factories.keys()
-
-
-class ParallelizerFactory(object):
-    """ A factory for finding MPI parallelizers."""
-
-    __recipefile__ = abspath(join(dirname(__file__), "mpi.yaml"))
-    __recipes__ = None
-
-    _factories = {
-        "srun": GeneralParallelizer,
-    }
-
-    @classmethod
-    def get_parallelizer(cls, mpi_type):
-        """
-        Get the Parallelizer for the specfied MPI type.
-
-        :param mpi_type: The MPI binary to parallelize with.
-        :returns: A Parallelizer object for the specified MPI type.
-        """
-        # Check that the requested parallel command is one we support.
-        if mpi_type.lower() not in cls.factories:
-            msg = "Parallelizer '{0}' not found. Specify an adapter that " \
-                  "exists or implement a new one mapping to the '{0}'" \
-                  .format(str(mpi_type))
-            LOGGER.error(msg)
-            raise Exception(msg)
-
-        # Check out factory for the object we need.
-        parallelizer = cls._factories[mpi_type]
-
-        # If we see the general case that uses recipes, do the following:
-        if isinstance(parallelizer, GeneralParallelizer):
-            # If we've not loaded the recipes, do so.
-            if cls.__recipes__ is None:
-                cls.__recipes__ = yaml.load(cls.__recipefile__)
-
-            # We also need to have the recipe for the MPI flavor we requested.
-            # If it's not in our recipes, we can continue -- abort.
-            if mpi_type not in cls.__recipes__:
-                msg = "'{0}' uses a generalized recipe but the recipe does " \
-                      "exist! Please make sure that your recipes are up to" \
-                      "date. Recipe file location = {1}" \
-                      .format(mpi_type, cls.__recipefile__)
-                LOGGER.exception(msg)
-                raise KeyError(msg)
-
-            # Otherwise, construct and return the general parallizer.
-            return parallelizer(cls.__recipes__[mpi_type])
-        else:
-            # Otherwise, we should just return the instance of the specific
-            # Parallelizer.
-            # NOTE: There is a glass jaw here. If a specific Parallelizer needs
-            # informattion for construction, we'll end up needing a case tree
-            # here to check for types.
-            return parallelizer()
-
-    @classmethod
-    def get_valid_parallelizers(cls):
-        """
-        Get the available MPI launchers.
-
-        :returns: A list of available MPI launchers.
-        """
-        return cls._factories.keys()
--- maestrowf/interfaces/mpi/__init__.py
+++ maestrowf/interfaces/mpi/__init__.py
@@ -28,6 +28,8 @@
 ###############################################################################
 """A module that provides interfaces to multiple MPI launchers."""
 import logging
+from os.path import abspath, dirname, join
+import yaml
 
 from maestrowf.abstracts.interfaces import Parallelizer
 
@@ -38,8 +40,88 @@ LOGGER = logging.getLogger(__name__)
 class GeneralParallelizer(Parallelizer):
     """A class for general parallel command generation."""
 
-    def __init__(self, parameters):
-        self._params = parameters
+    def __init__(self, cmd, recipe):
+        """
+        Construct an instance of the GeneralParallelizer.
+
+        :param cmd: The parallel command to be used.
+        :param recipe: A dictionary containing the generalized recipe format.
+        """
+        self._cmd = cmd
+        self._recipe = recipe
 
     def parallelize(self, cmd, resources):
+        """
+        Generate the parallelization segement of the command line.
+
+        :param cmd: Command string to substitute parallel command into.
+        :param resources: Dict of resources to be used by parallel commands.
+        :returns: A string of the parallelize command configured using the
+        specified resources.
+        """
         pass
+
+
+class ParallelizerFactory(object):
+    """A factory for finding MPI parallelizers."""
+
+    __recipefile__ = abspath(join(dirname(__file__), "recipes", "mpi.yaml"))
+    __recipes__ = None
+
+    _factories = {
+        "srun": GeneralParallelizer,
+    }
+
+    @classmethod
+    def get_parallelizer(cls, mpi_type):
+        """
+        Get the Parallelizer for the specfied MPI type.
+
+        :param mpi_type: The MPI binary to parallelize with.
+        :returns: A Parallelizer object for the specified MPI type.
+        """
+        # Check that the requested parallel command is one we support.
+        if mpi_type.lower() not in cls.factories:
+            msg = "Parallelizer '{0}' not found. Specify an adapter that " \
+                  "exists or implement a new one mapping to the '{0}'" \
+                  .format(str(mpi_type))
+            LOGGER.error(msg)
+            raise Exception(msg)
+
+        # Check out factory for the object we need.
+        parallelizer = cls._factories[mpi_type]
+
+        # If we see the general case that uses recipes, do the following:
+        if isinstance(parallelizer, GeneralParallelizer):
+            # If we've not loaded the recipes, do so.
+            if cls.__recipes__ is None:
+                cls.__recipes__ = yaml.load(cls.__recipefile__)
+
+            # We also need to have the recipe for the MPI flavor we requested.
+            # If it's not in our recipes, we can continue -- abort.
+            if mpi_type not in cls.__recipes__:
+                msg = "'{0}' uses a generalized recipe but the recipe does " \
+                      "exist! Please make sure that your recipes are up to" \
+                      "date. Recipe file location = {1}" \
+                      .format(mpi_type, cls.__recipefile__)
+                LOGGER.exception(msg)
+                raise KeyError(msg)
+
+            # Otherwise, construct and return the general parallizer.
+            return parallelizer(mpi_type, cls.__recipes__[mpi_type])
+        else:
+            # Otherwise, we should just return the instance of the specific
+            # Parallelizer.
+            # NOTE: There is a glass jaw here. If a specific Parallelizer needs
+            # informattion for construction, we'll end up needing a case tree
+            # here to check for types.
+            return parallelizer()
+
+    @classmethod
+    def get_valid_parallelizers(cls):
+        """
+        Get the available MPI launchers.
+
+        :returns: A list of available MPI launchers.
+        """
+        return cls._factories.keys()
