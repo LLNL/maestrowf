f6a0ddaedcea07410838fab8797e6de3d6cbdc89
Francesco Di Natale <frank.dinatale1988@gmail.com>
2018-08-01 09:26:49 -0700

    Correction to missing parameters in SIZE.

--- samples/parameterization/lulesh_custom_gen.py
+++ samples/parameterization/lulesh_custom_gen.py
@@ -20,18 +20,16 @@ def get_custom_generator():
             "label": "TRIAL.%%"
         },
         "SIZE": {
-            "values": [10, 10, 10, 20, 20, 20, 20],
+            "values": [10, 10, 10, 20, 20, 20, 30, 30, 30],
             "label": "SIZE.%%"
         },
         "ITERATIONS": {
             "values": [10, 20, 30, 10, 20, 30, 10, 20, 30],
             "label": "ITERATIONS.%%"
-        },
+        }
     }
 
     for key, value in params.items():
-        label = value["label"]
-        for v in value["values"]:
-            p_gen.add_parameter(key, v, label)
+        p_gen.add_parameter(key, value["values"], value["label"])
 
     return p_gen
