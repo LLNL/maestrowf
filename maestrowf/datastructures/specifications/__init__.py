46a4efe5d13dad9aa5a6129f798e7d75a21934d7
Francesco Di Natale <dinatale3@llnl.gov>
2018-10-21 12:47:10 -0700

    Addition of a factory for finding specifications.

--- maestrowf/datastructures/specifications/__init__.py
+++ maestrowf/datastructures/specifications/__init__.py
@@ -1,6 +1,63 @@
 """Module containing datastructures for parsing specifications."""
+from importlib import import_module
+from inspect import getmodule
+import logging
 
-from maestrowf.datastructures.specifications.yamlspecification import \
-    YAMLSpecification
+LOGGER = logging.getLogger(__name__)
 
-__all__ = ("YAMLSpecification")
+
+class SpecificationFactory(object):
+    """A factory class to retrieve different types of Specifications."""
+
+    _classes = {
+        "yaml": {
+            "latest":   (".yamlspecification", "YAMLSpecification"),
+            "1.0":      (".yamlspecification", "YAMLSpecification"),
+        },
+    }
+
+    @classmethod
+    def get_specification(cls, spec_name, version="latest"):
+        """
+        Look up and retrieve a Specification by name.
+
+        :param spec_name: Name of the Specification to find.
+        :returns: A Specification class matching the specifed spec_name.
+        """
+        if spec_name.lower() not in cls._classes:
+            msg = "Specification '{0}' not found. Specify an adapter that " \
+                  "exists." \
+                  .format(str(spec_name))
+            LOGGER.error(msg)
+            raise Keyerror(msg)
+
+        parent_module = getmodule(cls).__name__
+        module = cls._classes[spec_name][version]
+        return getattr(
+            import_module("{}{}".format(parent_module, module[0])),
+            module[1])
+
+    @classmethod
+    def get_valid_specifications(cls):
+        """
+        Get all valid ScriptAdapter names.
+
+        :returns: A list of all available keys in the ScriptAdapterFactory.
+        """
+        return cls._classes.keys()
+
+    @classmethod
+    def get_specifications_versions(cls, spec_name):
+        """
+        Get available versions of a specification type.
+
+        :returns: A list of all available versions of a Specification.
+        """
+        if spec_name not in cls._classes:
+            msg = "Specification '{0}' not found. Specify an adapter that " \
+                  "exists." \
+                  .format(str(spec_name))
+            LOGGER.error(msg)
+            raise KeyError(msg)
+
+        return cls._classes[spec_name].keys()
