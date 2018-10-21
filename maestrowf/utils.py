b221bf0afa0845650a931b8d6b5e6587c1c942a0
Francesco Di Natale <dinatale3@llnl.gov>
2018-10-20 17:29:17 -0700

    Addition of a utility method to create a dictionary from a list of key-value pairs.

--- maestrowf/utils.py
+++ maestrowf/utils.py
@@ -209,3 +209,19 @@ def ping_url(url):
     else:
         response.read()
         return
+
+
+def create_dictionary(list_keyvalues, token=":"):
+    """
+    Create a dictionary from a list of key-value pairs.
+
+    :param list_keyvalues: List of token separates key-values.
+    :param token: The token to split each key-value by.
+    :returns: A dictionary containing the key-value pairings in list_keyvalues.
+    """
+    _dict = {}
+    for item in list_keyvalues:
+        key, value = [i.strip() for i in item.split(token, 1)]
+        _dict[key] = value
+
+    return _dict
