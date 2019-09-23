4864f70b29b62ec27840316e53ceb374526ffca9
Francesco Di Natale <dinatale3@llnl.gov>
2019-09-23 11:38:01 -0700

    Update setup.py to 1.1.3dev

--- setup.py
+++ setup.py
@@ -1,46 +1,38 @@
 from maestrowf import __version__
 from setuptools import setup, find_packages
 
-setup(
-  name='maestrowf',
-  description='A tool and library for specifying and conducting general '
-  'workflows.',
-  version=__version__,
-  author='Francesco Di Natale',
-  maintainer='Francesco Di Natale',
-  author_email='dinatale3@llnl.gov',
-  url='https://github.com/llnl/maestrowf',
-  license='MIT License',
-  packages=find_packages(),
-  entry_points={
-    'console_scripts': [
-        'maestro = maestrowf.maestro:main',
-        'conductor = maestrowf.conductor:main',
-    ]
-  },
-  install_requires=[
-    'PyYAML>=4.2b1',
-    'six',
-    "filelock",
-    "tabulate",
-    "enum34 ; python_version<'3.4'"
-  ],
-  extras_require={},
-  long_description_content_type='text/markdown',
-  download_url='https://pypi.org/project/maestrowf/',
-  python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Operating System :: Unix',
-    'Operating System :: MacOS :: MacOS X',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Education',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering',
-    'Topic :: System :: Distributed Computing',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
-)
+setup(name='maestrowf',
+      description='A tool and library for specifying and conducting general '
+                  'workflows.',
+      version='1.1.4',
+      author='Francesco Di Natale',
+      author_email='dinatale3@llnl.gov',
+      url='https://github.com/llnl/maestrowf',
+      license='MIT License',
+      packages=find_packages(),
+      entry_points={
+        'console_scripts': [
+            'maestro = maestrowf.maestro:main',
+            'conductor = maestrowf.conductor:main',
+        ]
+      },
+      install_requires=[
+        'PyYAML>=4.2b1',
+        'six',
+        "filelock",
+        "tabulate",
+        ],
+      extras_require={
+        ":python_version<'3.4'": ['enum34'],
+      },
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+      )
