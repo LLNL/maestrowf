3cb9187f99f6bcd2aa619e699c7a9be4b5bf0f61
Francesco Di Natale <dinatale3@llnl.gov>
2020-01-16 13:27:15 -0800

    Addition of python requirements, download url, and maintainer.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
   'workflows.',
   version=__version__,
   author='Francesco Di Natale',
+  maintainer='Francesco Di Natale',
   author_email='dinatale3@llnl.gov',
   url='https://github.com/llnl/maestrowf',
   license='MIT License',
@@ -24,7 +25,9 @@ setup(
     "tabulate",
   ],
   extras_require={},
-  long_description_content_type="text/markdown",
+  long_description_content_type='text/markdown',
+  download_url='https://pypi.org/project/maestrowf/',
+  python_requires='~=3.5',
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Operating System :: Unix',
