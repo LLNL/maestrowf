b536835b41c0143d4313cba8abd17baca533d988
Francesco Di Natale <dinatale3@llnl.gov>
2020-01-16 12:55:54 -0800

    Removal of enum34 and Python 3.4 classifiers.
    PyYAML no longer supports Python 3.4 which is forcing Maestro to
    also drop support as it has a direct dependency.

--- setup.py
+++ setup.py
@@ -1,43 +1,42 @@
 from maestrowf import __version__
 from setuptools import setup, find_packages
 
-setup(name='maestrowf',
-      description='A tool and library for specifying and conducting general '
-      'workflows.',
-      version=__version__,
-      author='Francesco Di Natale',
-      author_email='dinatale3@llnl.gov',
-      url='https://github.com/llnl/maestrowf',
-      license='MIT License',
-      packages=find_packages(),
-      entry_points={
-        'console_scripts': [
-            'maestro = maestrowf.maestro:main',
-            'conductor = maestrowf.conductor:main',
-        ]
-      },
-      install_requires=[
-        'PyYAML>=4.2b1',
-        'six',
-        "filelock",
-        "tabulate",
-        "enum34 ; python_version<'3.4'"
-        ],
-      extras_require={},
-      long_description_content_type="text/markdown",
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Operating System :: Unix',
-        'Operating System :: MacOS :: MacOS X',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: System :: Distributed Computing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        ],
-      )
+setup(
+  name='maestrowf',
+  description='A tool and library for specifying and conducting general '
+  'workflows.',
+  version=__version__,
+  author='Francesco Di Natale',
+  author_email='dinatale3@llnl.gov',
+  url='https://github.com/llnl/maestrowf',
+  license='MIT License',
+  packages=find_packages(),
+  entry_points={
+    'console_scripts': [
+        'maestro = maestrowf.maestro:main',
+        'conductor = maestrowf.conductor:main',
+    ]
+  },
+  install_requires=[
+    'PyYAML>=4.2b1',
+    'six',
+    "filelock",
+    "tabulate",
+  ],
+  extras_require={},
+  long_description_content_type="text/markdown",
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Operating System :: Unix',
+    'Operating System :: MacOS :: MacOS X',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: System :: Distributed Computing',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
