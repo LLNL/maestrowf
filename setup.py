e26cfd63c27514bb2382979867c3a431112e546f
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-01-17 13:02:07 -0800

    Re-add py2.7. Note: py2.7 unofficially supported.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
   extras_require={},
   long_description_content_type='text/markdown',
   download_url='https://pypi.org/project/maestrowf/',
-  python_requires='~=3.5',
+  python_requires='~=2.7, ~=3.5',
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Operating System :: Unix',
