6f496c146cc33f3a01a82b932dde6f147466aea2
Chris Krenn <krenn1@llnl.gov>
2020-05-18 17:08:55 -0700

    edits to pass flake8

--- samples/multidimensional_sampling/pgen_sample.py
+++ samples/multidimensional_sampling/pgen_sample.py
@@ -1,15 +1,16 @@
 """This file implements several sampling methods"""
 
 import logging
+from maestrowf.datastructures.core import ParameterGenerator
 
 LOGGER = logging.getLogger(__name__)
 
-from maestrowf.datastructures.core import ParameterGenerator
 
-def _log_assert(test,msg):
+def _log_assert(test, msg):
     if not(test):
         LOGGER.error(msg)
         raise ValueError(msg)
+
 
 def _validate_constants_parameters(sampling_dict):
     _log_assert(
@@ -18,20 +19,23 @@ def _validate_constants_parameters(sampling_dict):
         "'parameters' or 'constants' must exist")
     if "constants" in sampling_dict.keys():
         _log_assert(
-            type(sampling_dict["constants"]) == type({"var":"val"}),
+            isinstance(sampling_dict["constants"], dict),
             "'constants' must be a dictionary")
+
 
 def _validate_parameters_dict(sampling_dict):
     if "parameters" in sampling_dict.keys():
         _log_assert(
-            type(sampling_dict["parameters"]) == type({"var":"val"}),
+            isinstance(sampling_dict["parameters"], dict),
             "'parameters' must be a dictionary for this sampling type")
-        
+
+
 def _validate_parameters_string(sampling_dict):
     if "parameters" in sampling_dict.keys():
         _log_assert(
-            type(sampling_dict["parameters"]) == str,
+            isinstance(sampling_dict["parameters"], str),
             "'parameters' must be a string for this sampling type")
+
 
 def _validate_sample_dict(samples):
     keys = list(samples[0].keys())
@@ -42,6 +46,7 @@ def _validate_sample_dict(samples):
                 ("data point " +
                  str(row) + " does not have a value for " +
                  str(key)))
+
 
 def _convert_dict_to_maestro_params(samples):
     _validate_sample_dict(samples)
@@ -54,6 +59,7 @@ def _convert_dict_to_maestro_params(samples):
         parameters[key]["values"] = values
     return parameters
 
+
 def _validate_list_dictionary(sampling_dict):
     _validate_constants_parameters(sampling_dict)
     _validate_parameters_dict(sampling_dict)
@@ -64,12 +70,15 @@ def _validate_list_dictionary(sampling_dict):
             _log_assert(type(value) == list, "parameter values must be a list")
             if first:
                 sample_length = len(value)
-            _log_assert(len(value) == sample_length, "all sample lists must be the same length")
+            _log_assert(
+                len(value) == sample_length,
+                "all sample lists must be the same length")
+
 
 def list_sample(sampling_dict):
     """
     Return set of samples based on specification in sampling_dict.
-    
+
     Prototype dictionary:
 
     sample_type: list
@@ -99,32 +108,34 @@ def list_sample(sampling_dict):
         samples.append(constants)
     return _convert_dict_to_maestro_params(samples)
 
-            
+
 def _validate_cross_product_dictionary(sampling_dict):
     _validate_constants_parameters(sampling_dict)
     if "parameters" in sampling_dict.keys():
         for key, value in sampling_dict["parameters"].items():
             _log_assert(type(key) == str, "parameter labels must be strings")
             _log_assert(type(value) == list, "parameter values must be a list")
-            
-def _recursive_cross_product_sample(params,samples=[{}]):
+
+
+def _recursive_cross_product_sample(params, samples=[{}]):
     if params == {}:
         return samples
     key = next(iter(params))
     new_list = []
     for sample in samples:
-        for item in params[key]: 
+        for item in params[key]:
             new_sample = sample.copy()
             new_sample[key] = item
             new_list.append(new_sample)
     new_params = params.copy()
     new_params.pop(key)
-    return _recursive_cross_product_sample(new_params,samples=new_list)    
-    
+    return _recursive_cross_product_sample(new_params, samples=new_list)
+
+
 def cross_product_sample(sampling_dict):
     """
     Return set of samples based on specification in sampling_dict.
-    
+
     Prototype dictionary:
 
     sample_type: cross_product
@@ -144,11 +155,14 @@ def cross_product_sample(sampling_dict):
         constants = {}
     if "parameters" in sampling_dict.keys():
         parameters = sampling_dict["parameters"].copy()
-        samples =  _recursive_cross_product_sample(parameters,samples=[constants])
+        samples = _recursive_cross_product_sample(
+                                            parameters,
+                                            samples=[constants])
     else:
-        samples=[constants]
-    LOGGER.info("samples:\n%s",str(samples))
+        samples = [constants]
+    LOGGER.info("samples:\n%s", str(samples))
     return _convert_dict_to_maestro_params(samples)
+
 
 def _validate_column_list_dictionary(sampling_dict):
     _validate_constants_parameters(sampling_dict)
@@ -158,7 +172,7 @@ def _validate_column_list_dictionary(sampling_dict):
 def column_list_sample(sampling_dict):
     """
     Return set of samples based on specification in sampling_dict.
-    
+
     Prototype dictionary:
 
     sample_type: column_list
@@ -185,48 +199,53 @@ def column_list_sample(sampling_dict):
             data = row.split()
             if len(data) > 0:
                 _log_assert(
-                    len(data)==len(headers),
-                    "Data >>"+str(data)+"<< does not match\n"+
-                    "headers >>"+str(headers)+"<<.")
-                sample=constants.copy()
-                for header, datum in zip(headers,data):
+                    len(data) == len(headers),
+                    "Data >>" + str(data) + "<< does not match\n" +
+                    "headers >>" + str(headers) + "<<.")
+                sample = constants.copy()
+                for header, datum in zip(headers, data):
                     sample[header] = datum
                 samples.append(sample)
     else:
         samples.append(constants)
     return _convert_dict_to_maestro_params(samples)
-    
+
+
 def get_custom_generator(env, **kwargs):
     """
     Create a custom populated ParameterGenerator.
 
-    This function supports several sampling methods. 
+    This function supports several sampling methods.
 
     :params kwargs: A dictionary of keyword arguments this function uses.
     :returns: A ParameterGenerator populated with parameters.
     """
     p_gen = ParameterGenerator()
     try:
-        SAMPLE_DICTIONARY = kwargs.get("sample_dictionary", env.find("SAMPLE_DICTIONARY").value)
-    except:
-        raise ValueError("this pgen code requires SAMPLE_DICTIONARY to be defined in the yaml specification")
+        SAMPLE_DICTIONARY = kwargs.get(
+            "sample_dictionary",
+            env.find("SAMPLE_DICTIONARY").value)
+    except ValueError:
+        raise ValueError("this pgen code requires SAMPLE_DICTIONARY " +
+                         "to be defined in the yaml specification")
     try:
         sample_type = SAMPLE_DICTIONARY["sample_type"]
-    except:
-        raise ValueError("this pgen code requires SAMPLE_DICTIONARY['sample_type'] " +
-                         "to be defined in the yaml specification")
+    except ValueError:
+        raise ValueError("this pgen code requires SAMPLE_DICTIONARY" +
+                         "['sample_type'] to be defined in the yaml " +
+                         "specification")
     if sample_type == "list":
         params = list_sample(SAMPLE_DICTIONARY)
-        LOGGER.info("params:\n%s",str(params))
+        LOGGER.info("params:\n%s", str(params))
     elif sample_type == "cross_product":
         params = cross_product_sample(SAMPLE_DICTIONARY)
-        LOGGER.info("params:\n%s",str(params))
+        LOGGER.info("params:\n%s", str(params))
     elif sample_type == "column_list":
         params = column_list_sample(SAMPLE_DICTIONARY)
-        LOGGER.info("params:\n%s",str(params))
+        LOGGER.info("params:\n%s", str(params))
     else:
-        raise ValueError("The 'sample_type' of "+sample_type+" is not supported.")
-
+        raise ValueError("The 'sample_type' of " + sample_type +
+                         " is not supported.")
 
     for key, value in params.items():
         p_gen.add_parameter(key, value["values"], value["label"])
