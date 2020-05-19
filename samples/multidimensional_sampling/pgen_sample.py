3a85c70e6bb6797e12caa2a8000c8bbe2d065769
Chris Krenn <krenn1@llnl.gov>
2020-05-18 23:45:52 -0700

    fixed flake8 errors

--- samples/multidimensional_sampling/pgen_sample.py
+++ samples/multidimensional_sampling/pgen_sample.py
@@ -1,27 +1,20 @@
 """This file implements several sampling methods"""
 
-import yaml
 import random
-import glob
-import time
-from math import *
-
 import logging
-from maestrowf.datastructures.core import ParameterGenerator
-
 from contextlib import suppress
+from maestrowf.datastructures.core import ParameterGenerator
 
 PANDAS_PLUS = False
 with suppress(ModuleNotFoundError):
     import pandas as pd
     import numpy as np
     import scipy.spatial as spatial
-    from sklearn.tree import DecisionTreeRegressor
-    from sklearn.neighbors import KDTree
     PANDAS_PLUS = True
 
 
 LOGGER = logging.getLogger(__name__)
+
 
 def _log_assert(test, msg):
     if not(test):
@@ -250,10 +243,11 @@ def _validate_best_candidate_dictionary(sampling_dict):
             str(value["max"]).isnumeric(),
             "parameter must have a numeric maximum")
 
+
 def down(samples, sampling_dict):
     """
     Downselect samples based on specification in sampling_dict.
-        
+
     Prototype dictionary:
 
     num_samples: 30
@@ -270,77 +264,77 @@ def down(samples, sampling_dict):
         PANDAS_PLUS,
         "This function requires pandas, numpy, scipy & sklearn packages")
     _validate_best_candidate_dictionary(sampling_dict)
-    
+
     df = pd.DataFrame.from_dict(samples)
-    columns=sampling_dict["parameters"].keys()
+    columns = sampling_dict["parameters"].keys()
     ndims = len(columns)
     candidates = df[columns].values.tolist()
     num_points = sampling_dict["num_samples"]
- 
+
     if not('previous_samples' in sampling_dict.keys()):
-        sample_points=[]
+        sample_points = []
         sample_points.append(candidates[0])
-        new_sample_points=[]
+        new_sample_points = []
         new_sample_points.append(candidates[0])
         new_sample_ids = []
         new_sample_ids.append(0)
         n0 = 1
     else:
         try:
-            previous_samples = pd.read_csv(sampling_dict["previous_samples"])  
+            previous_samples = pd.read_csv(sampling_dict["previous_samples"])
         except ValueError:
             raise ValueError("Error opening previous_samples datafile:" +
-                  sampling_dict["previous_samples"])
-        sample_points=previous_samples[columns].values.tolist()
-        new_sample_points=[]
+                             sampling_dict["previous_samples"])
+        sample_points = previous_samples[columns].values.tolist()
+        new_sample_points = []
         new_sample_ids = []
         n0 = 0
-        
+
     mins = np.zeros(ndims)
     maxs = np.zeros(ndims)
     for j in range(ndims):
         mins[j] = 1.0e30
         maxs[j] = -1.0e30
-    
+
     for i in range(len(candidates)):
         ppi = candidates[i]
         for j in range(ndims):
-            mins[j] = min(ppi[j],mins[j])
-            maxs[j] = max(ppi[j],maxs[j])
-    print("extrema for new input_labels: ",mins,maxs)
-    print("down sampling to %d best candidates from %d total points."%(
-        num_points,len(candidates)))
+            mins[j] = min(ppi[j], mins[j])
+            maxs[j] = max(ppi[j], maxs[j])
+    print("extrema for new input_labels: ", mins, maxs)
+    print("down sampling to %d best candidates from %d total points." % (
+        num_points, len(candidates)))
     bign = len(candidates)
-    bigtree = spatial.KDTree(candidates)
- 
-    for n in range(n0,num_points):
+
+    for n in range(n0, num_points):
         px = np.asarray(sample_points)
         tree = spatial.KDTree(px)
         j = bign
         d = 0.0
-        for i in range(1,bign):
+        for i in range(1, bign):
             pos = candidates[i]
             dist = tree.query(pos)[0]
             if dist > d:
                 j = i
                 d = dist
-        if j==bign:
+        if j == bign:
             raise ValueError("Something went wrong!")
         else:
             new_sample_points.append(candidates[j])
             sample_points.append(candidates[j])
             new_sample_ids.append(j)
-    
+
     new_samples_df = pd.DataFrame(columns=df.keys().tolist())
     for n in range(len(new_sample_ids)):
-        new_samples_df=new_samples_df.append(df.iloc[new_sample_ids[n]])
+        new_samples_df = new_samples_df.append(df.iloc[new_sample_ids[n]])
 
     return new_samples_df.to_dict(orient='records')
+
 
 def random_sample(sampling_dict):
     """
     Return set of random samples based on specification in sampling_dict.
-    
+
     Prototype dictionary:
 
     num_samples: 30
@@ -364,14 +358,15 @@ def random_sample(sampling_dict):
     for i in range(sampling_dict["num_samples"]):
         random_dictionary = {}
         for key, value in sampling_dict["parameters"].items():
-            random_dictionary[key] = min_dict[key] + random.random() * range_dict[key]
+            random_dictionary[key] = (
+                    min_dict[key] + random.random() * range_dict[key])
         random_list.append(random_dictionary)
     return(random_list)
 
 
 def best_candidate_sample(sampling_dict, over_sample_rate=10):
     """
-    Return set of best candidate samples based on specification in sampling_dict.
+    Return best candidate samples based on specification in sampling_dict.
 
     Prototype dictionary:
 
