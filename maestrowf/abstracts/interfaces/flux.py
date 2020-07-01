8836427ad1590f3fe3c538bc56bdd7ef1b1f0a80
Francesco Di Natale <dinatale3@llnl.gov>
2020-07-01 14:43:58 -0700

    Correction of the FluxInterface name to remove _

--- maestrowf/abstracts/interfaces/flux.py
+++ maestrowf/abstracts/interfaces/flux.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractclassmethod, abstractmethod, \
 from maestrowf.abstracts import Singleton
 
 
-class _FluxInterface(ABC):
+class FluxInterface(ABC):
 
     @abstractclassmethod
     def get_statuses(cls, joblist):
