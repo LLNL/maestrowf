0398cfd9fd6a356bbf536ad306b984f09bc7b7ef
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-08-18 12:44:20 -0700

    Changes to extra name and requirements.

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(
         "coloredlogs",
         "chainmap ; python_version<'3'",
     ],
-    extras_require={"networkx": ["networkx"],
-                    "matplotlib": ["matplotlib"],
-                    "pygraphviz": ["pygrapyviz"]},
+    extras_require={
+        "vis": ["networkx", "matplotlib", "pygraphviz"],
+    },
     long_description=load_readme(),
     long_description_content_type='text/markdown',
     download_url='https://pypi.org/project/maestrowf/',
