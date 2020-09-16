97a5f16d9fa99c509a8ef9eb3cb0a1653fd85ca3
Kevin Athey <athey1@llnl.gov>
2020-09-16 07:22:34 -0700

    YAMLSpecification Testing (#317)
    * initial testing on loading spec
    
    * black settings added
    
    * reformatted by black
    
    * added test for validation errors
    
    * added test to check for missing key in step
    
    * updated schema to add check for non-null variables
    
    * added check for study steps
    
    * added test for multiple dependencies of the same name
    
    * changed yaml spec testing for easier reading
    
    * added test of global parameters
    
    * fixed test execution to verify error thrown
    
    * added output_path test
    
    * formatting fix
    
    * added tests for steps generation and params generation
    
    * added tests for get_study_env
    
    * initial testing on loading spec
    
    * black settings added
    
    * reformatted by black
    
    * added test for validation errors
    
    * added test to check for missing key in step
    
    * updated schema to add check for non-null variables
    
    * added check for study steps
    
    * added test for multiple dependencies of the same name
    
    * changed yaml spec testing for easier reading
    
    * added test of global parameters
    
    * fixed test execution to verify error thrown
    
    * added output_path test
    
    * formatting fix
    
    * added tests for steps generation and params generation
    
    * added tests for get_study_env
    
    * updated to add testing for description setter
    
    * added test for spec.name setter
    
    * pulled spec_path code into a pytest fixture
    
    * added pytest and coverage config
    
    * added pytest-cov as a tox requirement
    
    * added report.xml to gitignore
    
    * changed line length for black
    
    * added coverage and junit reports to tox
    
    * Added coverage appending for test runs

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+
+
+@pytest.fixture
+def spec_path():
+    def load_spec(file_name):
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(dirpath, "specification", "test_specs", file_name)
+
+    return load_spec
