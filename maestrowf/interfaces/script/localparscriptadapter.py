a4bbd4a53603960c580f9ee9804c98033ac60769
Jeremy White <white242@llnl.gov>
2022-02-07 23:59:07 -0800

    Initial pass hooking up launcher token replacement, fix up broken inheritance

--- maestrowf/interfaces/script/localparscriptadapter.py
+++ maestrowf/interfaces/script/localparscriptadapter.py
@@ -31,6 +31,7 @@
 import logging
 import os
 import psutil
+import re
 import signal
 import uuid
 
@@ -43,7 +44,6 @@ from maestrowf.abstracts.enums import JobStatusCode, SubmissionCode, \
     CancelCode, State
 from maestrowf.interfaces.script import CancellationRecord, SubmissionRecord
 from maestrowf.abstracts.interfaces import SchedulerScriptAdapter
-from maestrowf.abstracts import Singleton
 from maestrowf.utils import start_process
 
 
@@ -61,6 +61,21 @@ class LocalParallelScriptAdapter(SchedulerScriptAdapter):
     tasks_lock = RLock()
     total_procs = 1
     avail_procs = 1
+
+    # # The var tag to look for to replace for parallelized commands.
+    # launcher_var = "$(LAUNCHER)"
+    # # Allocation regex and compilation
+    # # Keeping this one here for legacy.
+    # launcher_regex = re.compile(
+    #     re.escape(launcher_var) + r"\[(?P<alloc>.*)\]")
+
+    # # We can have multiple requested submission properties.
+    # # Legacy allocation of nodes and procs.
+    # legacy_alloc = r"(?P<nodes>[0-9]+),\s*(?P<procs>[0-9]+)"
+    # # Just allocate based on tasks.
+    # task_alloc = r"(?P<procs>[0-9]+)p"
+    # # Just allocate based on nodes.
+    # node_alloc = r"(?P<nodes>[0-9]+)n"
 
     def __init__(self, **kwargs):
         """
@@ -88,6 +103,16 @@ class LocalParallelScriptAdapter(SchedulerScriptAdapter):
         self.total_procs = self._batch['proc_count']
         self.avail_procs = self.total_procs
         self.executor = ThreadPoolExecutor(max_workers=self.total_procs)
+
+        # Setup initial no-op launcher parameters for default operation
+        self._cmd_flags = {
+            "cmd": "",
+            "ntasks": None,
+            "nodes": None,
+            "cores per task": None
+        }
+
+        self._extension = '.sh'  # read this from shell key if present?
 
     def __getstate__(self):
         """Helper for excluding threadpool from pickling"""
@@ -151,9 +176,203 @@ class LocalParallelScriptAdapter(SchedulerScriptAdapter):
             (default = 1).
         :returns: A string of the parallelize command configured using nodes
             and procs.
-        :NOTE: this is currently a dummy method -> rework to add user specified replacements later
+        :NOTE: this is currently a dummy method -> rework to add user specified
+               replacements later
+        :NOTE: need a mechanism to override these/set from outside adapter
         """
-        return ""
+        args = [
+            self._cmd_flags["cmd"],
+        ]
+
+        return "".join(args)
+
+    def _substitute_parallel_command(self, step_cmd, **kwargs):
+        """
+        Substitute parallelized segments into a specified command.
+
+        :param step_cmd: Command string to parallelize.
+        :param nodes: Total number of requested nodes.
+        :param procs: Total number of requested processors.
+        :returns: The new command with all allocations substituted.
+        """
+        err_msg = "{} attempting to allocate {} {} for a parallel call with" \
+                  " a maximum allocation of {}"
+
+        nodes = kwargs.get("nodes")
+        procs = kwargs.get("procs")
+        addl_args = dict(kwargs)
+        addl_args.pop("nodes")
+        addl_args.pop("procs")
+
+        LOGGER.debug("nodes=%s; procs=%s", nodes, procs)
+        # See if the command contains a launcher token in it.
+        alloc_search = list(re.finditer(self.launcher_regex, step_cmd))
+        if alloc_search:
+            # If we find that launcher nomenclature.
+            total_nodes = 0     # Total nodes we've allocated so far.
+            total_procs = 0     # Total processors we've allocated so far.
+            cmd = step_cmd      # The step command we'll substitute into.
+            for match in alloc_search:
+                LOGGER.debug("Found a match: %s", match.group())
+                _nodes = None
+                _procs = None
+                # Look for the allocation information in the match.
+                _alloc = match.group("alloc")
+                # Search for the legacy format.
+                _legacy = re.search(self.legacy_alloc, _alloc)
+                if _legacy:
+                    # nodes, procs legacy notation.
+                    _ = _alloc.split(",")
+                    _nodes = _[0]
+                    _procs = _[1]
+                    LOGGER.debug(
+                        "Legacy setup detected. (nodes=%s, procs=%s)",
+                        _nodes,
+                        _procs
+                    )
+                else:
+                    # We're dealing with the new style.
+                    # Make sure we only have at most one proc and node
+                    # allocation specified.
+                    if _alloc.count("p") > 1 or _alloc.count("n") > 1:
+                        msg = "cmd: {}\n Invalid allocations specified ({})." \
+                              " Number of nodes and/or procs must only be " \
+                              "specified once." \
+                              .format(step_cmd, _alloc)
+                        LOGGER.error(msg)
+                        raise ValueError(msg)
+
+                    if _alloc.count("p") < 1:
+                        msg = "cmd: {}\n Invalid allocations specified ({})." \
+                              " Processors/tasks must be specified." \
+                              .format(step_cmd, _alloc)
+                        LOGGER.error(msg)
+                        raise ValueError(msg)
+
+                    _nodes = re.search(self.node_alloc, _alloc)
+                    if _nodes:
+                        _nodes = _nodes.group("nodes")
+                    _procs = re.search(self.task_alloc, _alloc)
+                    if _procs:
+                        _procs = _procs.group("procs")
+
+                    LOGGER.debug(
+                        "New setup detected. (nodes=%s, procs=%s)",
+                        _nodes,
+                        _procs
+                    )
+
+                msg = []
+                # Check that the requested nodes are within range.
+                if _nodes:
+                    _ = int(_nodes)
+                    total_nodes += _
+                    if _ > nodes:
+                        msg.append(
+                            err_msg.format(
+                                match.group(), _nodes, "nodes", nodes
+                            )
+                        )
+                # Check that the requested processors is within range.
+                if _procs:
+                    _ = int(_procs)
+                    total_procs += _
+                    if _ > procs:
+                        msg.append(
+                            err_msg.format(
+                                match.group(), _procs, "procs", procs
+                            )
+                        )
+                # If we have constructed a message, raise an exception.
+                if msg:
+                    LOGGER.error(msg)
+                    raise ValueError(msg)
+
+                pcmd = self.get_parallelize_command(
+                    _procs, _nodes, **addl_args
+                )
+                cmd = cmd.replace(match.group(), pcmd)
+
+            # Verify that the total nodes/procs used is within maximum.
+            if total_procs > procs:
+                msg = "Total processors ({}) requested exceeds the " \
+                      "maximum requested ({})".format(total_procs, procs)
+                LOGGER.error(msg)
+                raise ValueError(msg)
+
+            if total_nodes > nodes:
+                msg = "Total nodes ({}) requested exceeds the " \
+                      "maximum requested ({})".format(total_nodes, nodes)
+                LOGGER.error(msg)
+                raise ValueError(msg)
+
+            return cmd
+        else:
+            # 3. Two smaller cases here. If we see the launcher token WITHOUT
+            # any parameters, replace it there with full nodes and procs.
+            # Otherwise, just return the command. A user may simply want to run
+            # an unparallelized code in a submission.
+            pcmd = self.get_parallelize_command(procs, nodes, **addl_args)
+            # Catch the case where the launcher token appears on its own
+            if self.launcher_var in step_cmd:
+                LOGGER.debug(
+                    "'%s' found in cmd. Substituting", self.launcher_var)
+                return step_cmd.replace(self.launcher_var, pcmd)
+            else:
+                LOGGER.debug("The command did not specify an MPI command.")
+                return step_cmd.replace(self.launcher_var, '')
+
+    def get_scheduler_command(self, step):
+        """
+        Generate the full parallelized command for use in a batch script.
+
+        :param step: A StudyStep instance.
+        :returns:
+            1. A Boolean value - True if command is to be scheduled, False
+            otherwise.
+            2. A string representing the parallelized batch command for the
+            specified step command.
+            3. A string representing the parallelized batch command for the
+            specified step restart command.
+        """
+        # We should never get a study step that doesn't have a run entry; but
+        # better to be safe.
+        if not step.run:
+            msg = "Malformed StudyStep. A StudyStep requires a run entry."
+            LOGGER.error(msg)
+            raise ValueError(msg)
+
+        # If the user is requesting nodes, we need to request the nodes and
+        # set up the command with scheduling.
+        _nodes = step.run.get("nodes", 0)
+        _procs = step.run.get("procs", 0)
+
+        to_be_scheduled = False  # Local parallel does not submit batch jobs
+
+        if _nodes or _procs:
+            cmd = self._substitute_parallel_command(
+                step.run["cmd"],
+                **step.run
+            )
+            LOGGER.debug("Running parallel command: %s", cmd)
+
+            # Also check for the restart command and parallelize it too.
+            restart = ""
+            if step.run["restart"]:
+                restart = self._substitute_parallel_command(
+                    step.run["restart"],
+                    **step.run
+                )
+                LOGGER.debug("Restart command: %s", cmd)
+            LOGGER.info("Running parallel workflow step '%s' locally.", step.name)
+        # Otherwise, just return the command. It doesn't need scheduling.
+        else:
+            LOGGER.info("Running workflow step '%s' locally.", step.name)
+            to_be_scheduled = False
+            cmd = step.run["cmd"]
+            restart = step.run["restart"]
+
+        return to_be_scheduled, cmd, restart
 
     def _state(self, fut):
         """
@@ -196,7 +415,8 @@ class LocalParallelScriptAdapter(SchedulerScriptAdapter):
         # THIS IS A HACK FOR NOW: make better use of get_scheduler_command later
         cmd = step.run["cmd"]
         restart = step.run["restart"]
-        to_be_scheduled, _, _ = self.get_scheduler_command(step)
+        # to_be_scheduled, _, _ = self.get_scheduler_command(step)
+        to_be_scheduled, cmd, restart = self.get_scheduler_command(step)
 
         fname = "{}.sh".format(step.name)
         script_path = os.path.join(ws_path, fname)
@@ -459,3 +679,23 @@ class LocalParallelScriptAdapter(SchedulerScriptAdapter):
             rm_func(add_tasks=None, rm_tasks=[jid])
 
         return task_callback
+
+    @property
+    def extension(self):
+        """
+        Returns the extension that generated scripts will use.
+
+        :returns: A string of the extension
+        """
+        return self._extension
+
+
+    # @property
+    # def key(self):
+    #     """
+    #     Return the key name for a ScriptAdapter..
+
+    #     This is used to register the adapter in the ScriptAdapterFactory
+    #     and when writing the workflow specification.
+    #     """
+    #     return self.key         # 
