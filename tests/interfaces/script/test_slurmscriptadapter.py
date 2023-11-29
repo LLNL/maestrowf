dfaa092e8a614734710be1e84c2de19c969b90b9
Jeremy White <white242@llnl.gov>
2023-11-29 15:53:12 -0800

    Add initial unit tests for squeue and sacct job checks on live schedulers

--- tests/interfaces/script/test_slurmscriptadapter.py
+++ tests/interfaces/script/test_slurmscriptadapter.py
@@ -34,9 +34,19 @@ This was created to verify existing functionality of the ScriptAdapterFactory
 as it was converted to dynamically load all ScriptAdapters using a namespace
 plugin methodology.
 """
+import logging
+import os
+import re
+import time
+
+from maestrowf.abstracts.enums import State, JobStatusCode
 from maestrowf.interfaces.script.slurmscriptadapter import SlurmScriptAdapter
 from maestrowf.interfaces import ScriptAdapterFactory
+from maestrowf.utils import start_process
 
+import pytest
+
+TESTLOGGER = logging.getLogger(__name__)
 
 def test_slurm_adapter():
     """
@@ -62,3 +72,93 @@ def test_slurm_adapter_in_factory():
     # for it by key
     assert(ScriptAdapterFactory.get_adapter(SlurmScriptAdapter.key) ==
            SlurmScriptAdapter)
+
+
+# Slurm fixtures for checking scheduler connectivity
+@pytest.mark.sched_slurm
+@pytest.fixture()
+def slurm_test_jobs():
+    """Spin up a couple sample jobs to test slurm connectivity"""
+
+    # Archive the squeue/sacct formats so we can change them from defaults to verify
+    # that user customizations don't break the adapter
+    orig_sacct_fmt = os.getenv('SACCT_FORMAT')
+    TESTLOGGER.warn("Original SACCT_FORMAT: %s", orig_sacct_fmt)
+    orig_squeue_fmt = os.getenv('SQUEUE_FORMAT')
+    TESTLOGGER.warn("Original SQUEUE_FORMAT: %s", orig_squeue_fmt)
+
+    # Test out a users custom fmt here which had broken things previously (squeue)
+    new_sacct_fmt = 'jobid,jobname,account,partition,nnodes,state,start,elapsed,timelimit,priority'
+    new_squeue_fmt = '%.7i %.8u %.8a %.9P %.5D %.2t %.19S %.8M %.10l %10Q'
+    
+    os.environ['SACCT_FORMAT'] = new_sacct_fmt
+    os.environ['SQUEUE_FORMAT'] = new_squeue_fmt
+    TESTLOGGER.warn("Override SACCT_FORMAT: %s", os.environ['SACCT_FORMAT'])
+    TESTLOGGER.warn("Override SQUEUE_FORMAT: %s", os.environ['SQUEUE_FORMAT'])
+    
+    jobids = []
+    test_cmds = ["echo 'Test Job {}';srun -n1 sleep 60".format(idx) for idx in range(1)]
+    for cmd in test_cmds:
+        p = start_process(['sbatch', f'--wrap={cmd}', '-n', '1'], #.format(test_cmd=cmd)],
+                          cwd=os.getcwd(),
+                          env=os.environ)
+        output, err = p.communicate()
+        retcode = p.wait()
+
+        if retcode == 0:
+            jobids.append(re.search('[0-9]+', output).group(0))
+        else:
+            print(f'Error submitting job. retcode: {retcode}, output: {output}, err: {err}')
+
+    yield jobids
+
+    # Cleanup   (NOTE: want to also try cancelling jobs here or just let them run out?)
+    if orig_sacct_fmt:
+        os.environ['SACCT_FORMAT'] = orig_sacct_fmt
+    else:
+        os.environ.pop('SACCT_FORMAT')
+
+    if orig_squeue_fmt:
+        os.environ['SQUEUE_FORMAT'] = orig_squeue_fmt
+    else:
+        os.environ.pop('SQUEUE_FORMAT')
+
+    TESTLOGGER.warn("Reverted SACCT_FORMAT: %s", os.getenv('SACCT_FORMAT'))
+    TESTLOGGER.warn("Reverted SQUEUE_FORMAT: %s", os.getenv('SQUEUE_FORMAT'))
+
+@pytest.mark.sched_slurm
+def test_slurm_check(slurm_test_jobs, caplog):
+    jobids = slurm_test_jobs
+    caplog.set_level(logging.DEBUG)
+
+    TESTLOGGER.warn("SACCT_FORMAT = %s", os.environ['SACCT_FORMAT'])
+    TESTLOGGER.warn("SQUEUE_FORMAT = %s", os.environ['SQUEUE_FORMAT'])
+
+    slurm_adapter = ScriptAdapterFactory.get_adapter(SlurmScriptAdapter.key)(
+        host='dummy_host',
+        bank='dummy_bank',
+        queue='dummy_queue',
+        nodes=''
+    )
+
+    # NOTE: sacct output is often blank with shorter sleep times. Maybe smarter
+    #       handling with auto retries if sacct empty but squeue is fine?
+    time.sleep(5)
+    status_dict = {jobid: None for jobid in jobids}
+    job_status_sacct = slurm_adapter._check_jobs_sacct(jobids, status_dict)
+    status_dict = {jobid: None for jobid in jobids}
+    job_status_squeue = slurm_adapter._check_jobs_squeue(jobids, status_dict)
+
+
+    failed_states = [State.FAILED, State.HWFAILURE]
+
+    TESTLOGGER.warn("Testing squeue statuses: %s", job_status_squeue[1])
+    assert job_status_squeue[1]
+    for jobid, jobstate in job_status_squeue[1].items():
+        assert jobstate not in failed_states and jobstate in State
+
+    TESTLOGGER.warn("Testing sacct statuses: %s", job_status_sacct[1])
+    assert job_status_sacct[1]  # Make sure it's not empty
+    for jobid, jobstate in job_status_sacct[1].items():
+        assert jobstate         # Catch none with better err msg
+        assert jobstate not in failed_states and jobstate in State
